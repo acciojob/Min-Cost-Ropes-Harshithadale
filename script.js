e925b472aeac0a1fcb12fea91f5f3c8ebdbf6469
Harshithadale <daleharshi6045@gmail.com>
2025-03-25 22:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort();
+	while(arr.length > 1){
+		let sum = arr[0]+arr[1];
+		arr.shift();
+		arr.shift()
+		arr.push(sum)
+		arr.sort
+	}
+	return arr[0]
+	
 }
 
 module.exports=mincost;
