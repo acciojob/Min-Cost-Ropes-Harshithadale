c5d04692c2fec7cac548b2ba8d93dfa88804bfe2
Harshithadale <daleharshi6045@gmail.com>
2025-03-25 23:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function mincost(arr) { 
-    arr.sort((a, b) => a - b);
-    while(arr.length > 1){
-        let sum = arr[0] + arr[1];
-        arr.shift();
-        arr.shift();
-        arr.push(sum);
-        arr.sort((a, b) => a - b);
-    }
-    return arr[0];
+function mincost(arr)
+{ 
+//write your code here
+// return the min cost
+	arr.sort((a,b) => a-b);
+	let cost = 0;
+	while (arr.length > 1) {
+		let sum = arr[0] + arr[1];
+		cost += sum
+		arr.slice(0,2).concat(sum).sort((a,b) => a-b);
+	}
+	return sum
 }
 
 module.exports=mincost;
